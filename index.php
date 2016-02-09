7125b6224142e4967133854adf30262e2acd467c
David Morgan <dlm225@g.harvard.edu>
2016-02-08 19:48:07 -0500

    Refresh sample kitten page with draft personal page

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-# Hello World
-<h1> Hello World! </h1>
-<h2> Welcome to my site</h2>
-This is a test.
-<img src="images/kitten.jpeg">
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>P1 - an Introduction</title>
+  <meta name="description" content="Project 1 - David Morgan">
+  <meta name="author" content="David Morgan">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  <h1> David Morgan </h1>
+  <img src="images/david.jpg" height=400 width=300>
+</body>
+</html>
