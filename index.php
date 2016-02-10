0f49f5c13d11ee083c66ef94731309955cb9fe03
David Morgan <dlm225@g.harvard.edu>
2016-02-09 19:46:55 -0500

    Added link placeholders and tweaked css

--- index.php
+++ index.php
@@ -14,7 +14,16 @@
 </head>
 
 <body>
-  <h1> David Morgan </h1>
-  <img src="images/david.jpg" height=300 width=250 alt="Photo of David Morgan">
+  <h1 class="center"> David Morgan </h1>
+  <img class="centerimg" src="images/david.jpg" height=300 width=250 alt="Photo of David Morgan">
+  <h2 class="center"> Project 2 </h2>
+  <h3 class="center"> View: [Link] </h3>
+  <h3 class="center"> GitHub: [Link] </h3>
+  <h2 class="center"> Project 3 </h2>
+  <h3 class="center"> View: [Link] </h3>
+  <h3 class="center"> GitHub: [Link] </h3>
+  <h2 class="center"> Project 4 </h2>
+  <h3 class="center"> View: [Link] </h3>
+  <h3 class="center"> GitHub: [Link] </h3>
 </body>
 </html>
